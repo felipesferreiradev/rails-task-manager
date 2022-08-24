b60c6be82daf41eb03783e5e1cae529b49e946c1
Felipe Ferreira <lfelipesferreira@gmail.com>
2022-08-24 15:51:39 +0200

    22 02 - Rails Task 3.0

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,18 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :create, :edit, :update,]
+
   def index
     @tasks = Task.all
   end
+
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
+
   def new
     @task = Task.new
   end
+
   def create
     @task = Task.new(task_params)
     @task.save # Will raise ActiveModel::ForbiddenAttributesError
@@ -16,18 +21,28 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
+  end
+
+  def destroy
+    # @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
   end
 
   private
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  get 'tasks', to: 'tasks#index', as: :tasks
-  # The `new` route needs to be placed before the `show` route
-  get 'tasks/new', to: 'tasks#new', as: :new
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  post 'tasks', to: 'tasks#create'
-  get "tasks/:id/edit", to: "tasks#edit"
-  patch "tasks/:id", to: "tasks#update"
+  resources :tasks
+  # # Defines the root path route ("/")
+  # # root "articles#index"
+  # get 'tasks', to: 'tasks#index', as: :tasks
+  # # The `new` route needs to be placed before the `show` route
+  # get 'tasks/new', to: 'tasks#new', as: :new
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
